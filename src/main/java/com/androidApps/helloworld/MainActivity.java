bb6a39b6f9041d403f521f44c6ef51f394f7224b
Osilamah Solomon IMHOMOH <31593442+Osi4lucy@users.noreply.github.com>
2020-02-04 23:10:06 +0100

    Add files via upload
    Errors corrected

--- src/main/java/com/androidApps/helloworld/MainActivity.java
+++ src/main/java/com/androidApps/helloworld/MainActivity.java
@@ -4,19 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
-        // creaate handler for the page2 button
+    // create handler for the page2 button
 
-        public void goToPage2(View view){
-            Intent intent = new Intent(this, Page2.class);
-            startActivity(intent);
-        }
+    public void nextPage(View view){
+        Intent intent = new Intent(this, Page2.class);
+        startActivity(intent);
     }
 }
